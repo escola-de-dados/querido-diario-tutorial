70d4e58bf451e17c63edcf0380c15a85e0f84f80
belisards <adrianobf@gmail.com>
2021-08-19 19:52:06 -0300

    add imgs

---
+++ sp_paulinia_v2.py
@@ -0,0 +1,33 @@
+
+import datetime
+# import scrapy
+from gazette.items import Gazette
+from gazette.spiders.base import BaseGazetteSpider
+
+
+class SpPauliniaSpider(BaseGazetteSpider):
+    name = "sp_paulinia"
+    TERRITORY_ID = "2905206"
+    start_date = datetime.date(2010, 1, 4)
+    allowed_domains = ["www.paulinia.sp.gov.br/"]
+    start_urls = ["http://www.paulinia.sp.gov.br/semanarios/"]
+
+    def parse(self, response):
+        editions = response.xpath(
+            "//div[@class='container body-content']//div[@class='row']//a[contains(@href, 'AbreSemanario')]"
+        )
+        for edition in editions:
+            import pdb; pdb.set_trace()
+            final_url = edition.xpath("./@href").get()
+            link = "http://www.paulinia.sp.gov.br/semanarios/" + final_url
+            # description = edition.xpath(
+            #     "./text()"
+            # ).get()
+            # isso retorna string como "29/01/2021 - 1582 - Edição Normal" - falta ainda separar cada info
+
+            yield Gazette(
+                # edition_number = ,
+                file_urls=[link],
+                power="executive",
+            )
+ 
